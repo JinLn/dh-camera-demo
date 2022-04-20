b624ea03d22873391b76647ad8e66c56094a44a0
JinLn <2637838007@qq.com>
2022-04-20 20:44:59 +0800

    更新

--- main2cam.cpp
+++ main2cam.cpp
@@ -5,24 +5,39 @@
 #include <unistd.h>
 #include <iostream>
 #include <ctime>
-
 #include <opencv2/core.hpp>
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgproc.hpp>
-
 #include "easylogging++/easylogging++.h"
 #include "dh_camera.h"
+#include <termio.h>
+#include <fcntl.h>
 
 using namespace std;
+
 void Savestereoimg(cv::Mat leftimg, cv::Mat rightimg);
+int kbhit(void);
 
 int main() {
-    bool isSaveimg=false;
-    char getchar;
-    cout<<"Do you want to save the image？[y/n]"<<endl;
-    cin>>getchar;
-    if(getchar == 'y'){
-        isSaveimg=true;
+    bool isCalib = false, isSaveimg = false;
+    char getcalibchar, getsavechar;
+
+    cout << "Do you want to obtain images for calibration？[y/n]" << endl;
+    cin >> getcalibchar;
+    if (getcalibchar == 'y') {
+        isCalib = true;
+        cout << "Press press 'W' or 'w' to write the image !" << endl;
+    }
+
+    if (!isCalib) {
+        cout << "Do you want to save images continuously？[y/n]" << endl;
+        cin >> getsavechar;
+        if (getsavechar == 'y') {
+            isSaveimg = true;
+        }
+        else{
+            return 0;
+        }
     }
 
     DHCamera camleft = DHCamera();
@@ -55,36 +70,18 @@ int main() {
             --frame_count_right;
             continue;
         }
-        /*保存图像置为true*/
-        if(isSaveimg){
-            try{
+        if (kbhit() && isCalib) {
+            char ch = getchar();
+            if (ch == 'w'||ch == 'W') {
                 Savestereoimg(imgleft, imgright);
-            } catch (...) {
-                LOG(INFO) << "Save img error...";
             }
+            if (ch == 'q'||ch == 'Q')
+                break;
         }
 
-        try {
-            cv::hconcat(imgleft, imgright, imghconcat);
-            cv::putText(imghconcat,
-                        std::to_string(frame_count_left) + " FPS: " + std::to_string(fps_left),
-                        cv::Point(0, 24),
-                        cv::FONT_HERSHEY_DUPLEX,
-                        1,
-                        cv::Scalar(0, 255, 0),
-                        1,
-                        false);
-            cv::putText(imghconcat,
-                        std::to_string(frame_count_right) + " FPS: " + std::to_string(fps_right),
-                        cv::Point(imghconcat.cols / 2, 24),
-                        cv::FONT_HERSHEY_DUPLEX,
-                        1,
-                        cv::Scalar(0, 0, 255),
-                        1,
-                        false);
-            cv::imshow("CAM_Double", imghconcat);
-        } catch (...) {
-            LOG(INFO) << "Image error...";
+        /*连续保存图像置为true*/
+        if (isSaveimg) {
+            Savestereoimg(imgleft, imgright);
         }
 
         end_time = time(nullptr);
@@ -99,9 +96,8 @@ int main() {
             last_second_frame_cont_right = frame_count_right;
             start_time = time(nullptr);
         }
-
-
     }
+
     imgleft.release();
     imgright.release();
     camleft.StopStream();
@@ -111,20 +107,41 @@ int main() {
     return 0;
 }
 
-int i=0;
-void Savestereoimg(cv::Mat leftimg, cv::Mat rightimg){
-    char nameleft[150] = {},nameright[150] = {};
+static int imgnums = 0;
+
+void Savestereoimg(cv::Mat leftimg, cv::Mat rightimg) {
+    char nameleft[150] = {}, nameright[150] = {};
     string folderPathl = "../saveimg/leftimg";
     string folderPathr = "../saveimg/rightimg";
-    string commandl,commandr;
+    string commandl, commandr;
     commandl = "mkdir -p " + folderPathl;
     commandr = "mkdir -p " + folderPathr;
     system(commandl.c_str());
     system(commandr.c_str());
 
-    sprintf(nameleft, "../saveimg/leftimg/%06d.png", i);
-    sprintf(nameright, "../saveimg/rightimg/%06d.png", i++);
+    sprintf(nameleft, "../saveimg/leftimg/%06d.png", imgnums);
+    sprintf(nameright, "../saveimg/rightimg/%06d.png", imgnums++);
     imwrite(nameleft, leftimg);
     imwrite(nameright, rightimg);
-    LOG(INFO) << "Save stereo images "<< nameleft<<"  "<<nameright;
+    LOG(INFO) << "Save stereo images " << nameleft << "  " << nameright;
+}
+
+int kbhit(void) {
+    struct termios oldt, newt;
+    int ch;
+    int oldf;
+    tcgetattr(STDIN_FILENO, &oldt);
+    newt = oldt;
+    newt.c_lflag &= ~(ICANON | ECHO);
+    tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+    oldf = fcntl(STDIN_FILENO, F_GETFL, 0);
+    fcntl(STDIN_FILENO, F_SETFL, oldf | O_NONBLOCK);
+    ch = getchar();
+    tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+    fcntl(STDIN_FILENO, F_SETFL, oldf);
+    if (ch != EOF) {
+        ungetc(ch, stdin);
+        return 1;
+    }
+    return 0;
 }
